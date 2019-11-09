e9cbf2d52a0a94f00192b68c8e2602d828fef5c5
franna.jaya@gmail.com <franna.jaya@gmail.com>
2019-11-09 16:26:34 +0700

    [CHANGE] add 2 more activities, create main button function

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/frannajaya/racingtracker/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/frannajaya/racingtracker/MainActivity.java
@@ -6,44 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.snackbar.Snackbar;
-
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        Button newRaceButton = findViewById(R.id.new_race);
 
-        newRaceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "newRacePressed", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        Button savedRaceButton = findViewById(R.id.saved_race);
+        Button newRaceButton = findViewById(R.id.new_race_button);
+        newRaceButton.setOnClickListener(new nrButtonListener());
 
-        savedRaceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "savedRacePressed", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Button savedRaceButton = findViewById(R.id.saved_race_button);
+        savedRaceButton.setOnClickListener(new srButtonListener());
+    }
+    class nrButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent myIntent = new Intent(view.getContext(), CreateNewRaceActivity.class);
+            startActivityForResult(myIntent, 0);
+        }
     }
 
-    // Method handle go to Create race pressed.
-    public void newRacePressed(View view) {
-        Intent myIntent = new Intent(view.getContext(), CreateNewRaceActivity.class);
-        startActivityForResult(myIntent, 0);
-    }
-
-    public void savedRacePressed(View view) {
-        Intent myIntent = new Intent(view.getContext(), RaceListActivity.class);
-        startActivityForResult(myIntent, 0);
+    class srButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Intent myIntent = new Intent(view.getContext(), RaceListActivity.class);
+            startActivityForResult(myIntent, 0);
+        }
     }
 }
